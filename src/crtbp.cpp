d1d2340d830578633853ab8e2e21b12386716038
ProtossProbe <puluobi@gmail.com>
2017-07-05 23:15:13 +0800

    some changes

--- src/crtbp.cpp
+++ src/crtbp.cpp
@@ -20,22 +20,27 @@ using namespace std;
 using namespace boost::math;
 using namespace boost::numeric::odeint;
 
-class CRTBP {
+class CRTBP
+{
   public:
     CRTBP() = default;
     ~CRTBP() = default;
     CRTBP(double mu) : mu(mu) {}
+    CRTBP(double mu, char option) : mu(mu), option(option) {}
     const double mu = 0.001;
+    const char option = 'p';
     vec4 vector = {{0, 0, 0, 0}};
     vec4 elements = {{0, 0, 0, 0}};
-    ofstream outputfile1, outputfile2, outputfile3, outputfile4;
+    ofstream outputfile1, outputfile2, outputfile3, outputfile4, outputfile5;
 
-    class planar_crtbp_ode {
+    class planar_crtbp_ode
+    {
         double mu;
 
       public:
         planar_crtbp_ode(double mu) : mu(mu) {}
-        void operator()(const vec4 &x, vec4 &dxdt, double t) {
+        void operator()(const vec4 &x, vec4 &dxdt, double t)
+        {
             double r1, r2;
             double xmu = x[0] + mu;
             r1 = sqrt(pow(xmu, 2) + pow(x[1], 2));
@@ -51,7 +56,8 @@ class CRTBP {
         }
     };
 
-    double jacobi_inte(const vec4 &x) {
+    double jacobi_inte(const vec4 &x)
+    {
         double xmu = x[0] + mu;
         double y2 = pow(x[1], 2);
         double r1, r2;
@@ -61,7 +67,26 @@ class CRTBP {
                2 * (1 - mu) / r1 + 2 * mu / r2;
     }
 
-    void integ_output(vec4 x, const double endtime, const double dt = 0.01) {
+    double jacobi_inte_ele(const vec4 &elements)
+    {
+        double a = elements[0];
+        double e = elements[1];
+        double result = 0.;
+        if (option == 'p')
+        {
+            result = 1 / a + 2 * sqrt(a * (1 - e * e));
+        }
+        else if (option == 'r')
+        {
+            result = 1 / a - 2 * sqrt(a * (1 - e * e));
+        }
+        return result;
+    }
+
+    double jacobi2e(double C, double a) {}
+
+    void integ_output(vec4 x, const double endtime, const double dt = 0.01)
+    {
         outputfile1.open("../assets/rot.txt");
         outputfile2.open("../assets/init.txt");
         outputfile3.open("../assets/elements.txt");
@@ -71,14 +96,19 @@ class CRTBP {
         double current_t = 0.0;
         vec4 x_temp;
         write_keypoint(x, current_t);
-        while (current_t < endtime) {
+        while (current_t < endtime)
+        {
             x_temp = x;
-            // write_detail(x, current_t);
             stepper.do_step(planar_crtbp_ode(mu), x, 0.0, dt);
-            // if (rot2r(x_temp) < 0 and rot2r(x) > 0)
             current_t += dt;
-            if ((int)(current_t / dt) % 250 == 0)
+            if (rot2r(x_temp) < 0 and rot2r(x) > 0)
+            {
                 write_keypoint(x, current_t);
+            }
+            // if (int(current_t / dt) % 50 == 0) {
+            //     write_keypoint(x, current_t);
+            // }
+            write_detail(x, current_t);
         }
 
         outputfile1.close();
@@ -87,47 +117,56 @@ class CRTBP {
         outputfile4.close();
     }
 
-    void write_keypoint(const vec4 &x, const double t) {
+    void write_keypoint(const vec4 &x, const double t)
+    {
         outputfile4 << t << '\t';
         auto y = rot2init(x, t);
         auto elements = vector2elements(y);
         // double lambda = elements[2] + elements[3];
         double lambda_p = t;
-        double phi = 2 * (lambda_p - elements[3]) - elements[2];
-        if (phi > pi or phi < -pi) {
+        double phi = (elements[2] - elements[3] + t);
+        double psi = atan2(x[1], x[0]);
+        if (phi > pi or phi < -pi)
+        {
             int n = floor(phi / (2 * pi));
             phi -= n * 2 * pi;
         }
-        outputfile4 << elements[0] << '\t' << elements[1] << '\t' << phi
-                    << endl;
+        outputfile4 << elements[0] << '\t' << elements[1] << '\t' << phi << '\t'
+                    << psi << endl;
     }
 
-    void write_detail(const vec4 &x, const double t) {
+    void write_detail(const vec4 &x, const double t)
+    {
         outputfile1 << t << '\t';
         outputfile2 << t << '\t';
         outputfile3 << t << '\t';
         auto y = rot2init(x, t);
         auto elements = vector2elements(y);
-        for (auto ele : x) {
+        for (auto ele : x)
+        {
             outputfile1 << ele << '\t';
         };
-        for (auto ele : y) {
+        for (auto ele : y)
+        {
             outputfile2 << ele << '\t';
         };
-        for (auto ele : elements) {
+        for (auto ele : elements)
+        {
             outputfile3 << ele << '\t';
         };
         double E = jacobi_inte(x);
+        double E2 = jacobi_inte_ele(elements);
         outputfile1 << E << '\t' << endl;
         outputfile2 << endl;
-        outputfile3 << endl;
+        outputfile3 << E2 << '\t' << endl;
 
         // cout << '\t' << t << '\t' << x[0] << '\t' << x[1] << '\t' << E <<
         // '\t'
         //      << endl;
     }
 
-    vec4 rot2init(vec4 x, const double t) {
+    vec4 rot2init(vec4 x, const double t)
+    {
         double c = cos(t);
         double s = sin(t);
         x[0] += mu;
@@ -137,11 +176,12 @@ class CRTBP {
         result[1] = s * x[0] + c * x[1];
         result[2] = -s * (x[0] + x[3]) + c * (x[2] - x[1]);
         result[3] = s * (x[2] - x[1]) + c * (x[0] + x[3]);
-
         return result;
     }
 
-    vec4 init2rot(vec4 x, const double t) {
+    vec4 init2rot(vec4 x, const double t)
+    {
+
         double c = cos(t);
         double s = sin(t);
         vec4 result;
@@ -154,41 +194,58 @@ class CRTBP {
         return result;
     }
 
-    vec4 vector2elements(const vec4 &x) {
+    double rot2r(vec4 x) { return x[0] * x[2] + x[1] * x[3]; }
+
+    vec4 vector2elements(const vec4 &x)
+    {
         // input: {{x,y,vx,vy}}
         // output: {{a,e,M,omega}}
         double r = sqrt(x[0] * x[0] + x[1] * x[1]);
-        double vr = (x[0] * x[2] + x[1] * x[3]) / r;
-        double v2 = x[2] * x[2] + x[3] * x[3];
-        double vr2 = vr * vr;
         double h = x[0] * x[3] - x[2] * x[1];
-        double co1 = v2 - (1 - mu) / r;
-        double co2 = r * vr;
-        double ex = 1 / (1 - mu) * (co1 * x[0] - co2 * x[2]);
-        double ey = 1 / (1 - mu) * (co1 * x[1] - co2 * x[3]);
-        double omega = atan2(ey, ex);
-        double e = sqrt(ex * ex + ey * ey);
+        double angle = atan2(x[1], x[0]);
+        double vr = x[2] * cos(angle) + x[3] * sin(angle);
+        double vv = x[3] * cos(angle) - x[2] * sin(angle);
+        double theta = atan2(vr, vv - (1 - mu) / h);
+        double omega = angle - theta;
+        // cout << angle << '\t' << theta << '\t' << omega << '\t' << endl;
+        // cout << vr << '\t' << vv << endl;
+        double e =
+            sqrt(pow(vv * h / (1 - mu) - 1, 2) + pow(vr * h / (1 - mu), 2));
         double a = h * h / (1 - mu) / (1 - e * e);
-        double theta;
-        double temp = (h * h / ((1 - mu) * r) - 1) / e;
-        if (temp >= 1.) {
-            theta = 0.0;
-        } else {
-            theta = acos(temp);
-        }
-        if (vr < 0) {
-            theta = 2 * pi - theta;
-        }
+
+        // double vr = (x[0] * x[2] + x[1] * x[3]) / r;
+        // double v2 = x[2] * x[2] + x[3] * x[3];
+        // double vr2 = vr * vr;
+
+        // double co1 = v2 - (1 - mu) / r;
+        // double co2 = r * vr;
+        // double ex = 1 / (1 - mu) * (co1 * x[0] - co2 * x[2]);
+        // double ey = 1 / (1 - mu) * (co1 * x[1] - co2 * x[3]);
+        // double omega = atan2(ey, ex);
+        // double e = sqrt(ex * ex + ey * ey);
+        // double a = h * h / (1 - mu) / (1 - e * e);
+        // double theta;
+        // double temp = (h * h / ((1 - mu) * r) - 1) / e;
+        // if (temp >= 1.) {
+        //     theta = 0.0;
+        // } else {
+        //     theta = acos(temp);
+        // }
+        // if (vr < 0) {
+        //     theta = 2 * pi - theta;
+        // }
         double M = true2mean(theta, e);
         return {{a, e, M, omega}};
     }
 
-    double true2mean(double theta, double e) {
+    double true2mean(double theta, double e)
+    {
         double E = 2 * atan(sqrt((1 - e) / (1 + e)) * tan(theta / 2));
         return E - e * sin(E);
     }
 
-    vec4 elements2vector(const vec4 &elements) {
+    vec4 elements2vector(const vec4 &elements)
+    {
         // input: {{a,e,theta,omega}}
         // output:: {{x,y,vx,vy}}
         double a, e, theta, omega, h, r, angle, x, y, vx, vy, vv, vr;
@@ -203,36 +260,59 @@ class CRTBP {
         y = r * sin(angle);
         vv = (1 - mu) / h * (1 + e * cos(theta));
         vr = (1 - mu) / h * e * sin(theta);
-        vx = vr * cos(angle) + vv * sin(angle);
-        vy = -vr * sin(angle) + vv * cos(angle);
-
-        return {{x, y, vx, vy}};
+        vx = vr * cos(angle) - vv * sin(angle);
+        vy = vr * sin(angle) + vv * cos(angle);
+        if (option == 'p')
+        {
+            return {{x, y, vx, vy}};
+        }
+        else if (option == 'r')
+        {
+            return {{x, y, -vx, -vy}};
+        }
+        else
+        {
+            return {{0, 0, 0, 0}};
+        }
     }
 
-    void print_vec(const vec4 &x) {
-        for (auto ele : x) {
+    void print_vec(const vec4 &x)
+    {
+        for (auto ele : x)
+        {
             cout << ele << '\t';
         }
         cout << endl;
     }
 
-    void cal_init_vec() {
+    void cal_init_vec()
+    {
         auto x = elements2vector(elements);
         vector = init2rot(x, 0);
     }
 
-    void test() {
+    void test()
+    {
         double t = 0.0;
         print_vec(elements);
-        auto x = elements2vector(elements);
+        cal_init_vec();
+        auto x = vector;
         print_vec(x);
-        auto test_vec = init2rot(x, t);
-        print_vec(test_vec);
-        auto elements = vector2elements(rot2init(test_vec, t));
+        auto x2 = rot2init(x, t);
+        print_vec(x2);
+        auto elements = vector2elements(x2);
         print_vec(elements);
+
+        // cout << "Another Test !!!!" << endl;
+        // vec4 test = {{1, 0, 0, 1}};
+        // auto test2 = init2rot(test, 0);
+        // test = rot2init(test2, 0);
+        // print_vec(test);
+        // print_vec(test2);
     }
 
-    double delta21(double a) {
+    double delta21(double a)
+    {
         double fs1 = 0.38762717;
         double fd = -1.19049274;
         double mum = mu / (1 - mu);
@@ -243,27 +323,40 @@ class CRTBP {
         return alpha * pow(4 / e_beta, 1. / 3.);
     }
 
-    double Hamiltion21(double delta, double Phi, double phi) {
+    double Hamiltion21(double delta, double Phi, double phi)
+    {
         return delta * Phi + Phi * Phi - 2 * sqrt(2 * Phi) * cos(phi);
     }
 
-    double deltax1x2(double x1, double x2) {
+    double deltax1x2(double x1, double x2)
+    {
         return -(0.5 * (x2 * x2 + x1 * x1) + 4 / (x1 + x2));
     }
 
   private:
 };
 
-int main() {
-    double mu = 0.001;
-    CRTBP system(mu);
-    double a_res = pow(0.5, 2. / 3.);
-    double e = 0.01;
+class DistFunc
+{
+  public:
+    DistFunc() = default;
+    ~DistFunc() = default;
 
-    system.elements = {{a_res, e, 0, 0}};
+};
+
+int main()
+{
+    double mu = 0.001;
+    CRTBP system(mu, 'r');
+    double a_res = pow(0.5, 2. / 3.);
+    a_res = 1.0167;
+    double e = 0.055;
+
+    system.elements = {{a_res, e, 0.0, 0}};
     system.cal_init_vec();
-    // cout << "delta: " << system.delta21(a_res) << endl;
-    // cout << "delta: " << system.deltax1x2(2.21, 2.35) << endl;
-    system.integ_output(system.vector, 500 * pi, 0.001);
+    system.print_vec(system.vector);
+    // cout << "delta: " << system.deltax1x2(1.57, -0.45) << endl;
+    system.integ_output(system.vector, 1000 * pi, 0.005);
+    // system.test();
     cout << "DONE!" << endl;
 }
--- src/plot.py
+++ src/plot.py
@@ -33,22 +33,86 @@ def orbit_plot(y, range=0.5):
     plt.show()
 
 
-target = ["../assets/rot.txt", "../assets/init.txt",
-          "../assets/elements.txt", "../assets/key.txt"]
+def contour_plot(target):
+    fig, ax = plt.subplots()
+    # 指定绘图范围
+    ax.set_xlim(-0.5, 0.5)
+    ax.set_ylim(-0.5, 0.5)
+    for file in target:
+        data = np.loadtxt(file)
+        ax.scatter(data[:, 2] * np.cos(data[:, 3] / 2),
+                   data[:, 2] * np.sin(data[:, 3] / 2), s=3)
+    ax.grid(linestyle='dashed')
+    plt.show()
 
-# data2 = np.loadtxt(target[2])
+
+target = ["../assets/rot.txt", "../assets/init.txt",
+          "../assets/elements.txt", "../assets/key.txt", "../assets/key3.txt"]
+
+target2 = ["../assets/170629/-1.03 | 0.06.txt",
+           "../assets/170629/-1.03 | 0.06-.txt",
+           "../assets/170629/-1.03 | 0.15.txt",
+           "../assets/170629/-1.03 | 0.15-.txt",
+           "../assets/170629/-1.03 | 0.2.txt",
+           "../assets/170629/-1.03 | 0.03-.txt",
+           "../assets/170629/-1.03 | 0.1-.txt",
+           "../assets/170629/-1.03 | 0.08.txt",
+           "../assets/170629/-1.03 | 0.055.txt"]
+
+contour_plot(target2)
+
+data1 = np.loadtxt(target[0])
+data5 = np.loadtxt(target[4])
+data4 = np.loadtxt(target[3])
 # plt.plot(data2[:, 0], data2[:, 1])
 # plt.show()
+# data1 = np.loadtxt(target[2])
+# plt.plot(data1[:1000, 1], data1[:1000, 2])
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
+fig3, ax3 = plt.subplots()
+ax1.set_xlim(-1.5, 1.5)
+ax1.set_ylim(-1.5, 1.5)
+ax3.set_xlim(-0.5, 0.5)
+ax3.set_ylim(-0.5, 0.5)
+# for file in target:
+n = 1256
+# ax1.plot(data1[:, 1], data1[:, 2], linewidth=0.1)
+# ax1.plot(data1[:n, 1], data1[:n, 2], 'r', linewidth=2)
+# plot_primaries(ax1)
+# ax2.plot(data2[:, 1], data2[:, 2], linewidth=0.5)
+# ax3.scatter(data4[:, 2] * np.cos(data4[:, 3]),
+# data4[:, 2] * np.sin(data4[:, 3]), s=1)
+# ax2.plot(data5[:, 0], data5[:, 3])
+# ax2.plot(data4[:, 0], data4[:, 3])
 
-data3 = np.loadtxt(target[3])
-data3[:, 3] = np.unwrap(data3[:, 3])
-ax = plt.subplot(111)
+# ax2.plot(data2[:, 0], np.unwrap(data2[:, 3] -
+#                                 data2[:, 4] + data2[:, 0]), linewidth=1)
 
-ax.set_xlim(-4, 4)
-ax.set_ylim(-4, 4)
+# ax3.scatter(data5[:, 2] * np.cos(data5[:, 3]),
+#             data5[:, 2] * np.sin(data5[:, 3]), s=0.2)
+ax3.scatter(data4[:, 2] * np.cos(data4[:, 3]),
+            data4[:, 2] * np.sin(data4[:, 3]), s=0.5)
 
-ax.scatter(15.874 * data3[:, 2] * np.cos(data3[:, 3]),
-           15.874 * data3[:, 2] * np.sin(data3[:, 3]), s=0.5)
-# plt.plot(data3[:, 0], data3[:, 1])
-ax.grid(linestyle='dashed')
+# ax1.scatter(data1[:, 4] * 180 / np.pi, data1[:, 1], s=0.5)
+# ax2.scatter(data2[:, 2] * np.cos(data2[:, 3]),
+#             data2[:, 2] * np.sin(data2[:, 3]), s=0.5)
+# ax1.grid(linestyle='dashed')
+# ax2.grid(linestyle='dashed')
+ax3.grid(linestyle='dashed')
+# plt.show()
+
+# data3 = np.loadtxt(target[3])
+# data4 = np.loadtxt(target[4])
+# # data3[:, 3] = np.unwrap(data3[:, 3])
+# # data3[:, 4] = np.unwrap(data3[:, 4])
+# fig1, ax = plt.subplots()
+# fig2, ax2 = plt.subplots()
+
+
+# ax.scatter(data3[:, 4] * 180 / np.pi, data3[:, 1], s=0.5)
+# # plt.plot(data1[:, 0], data1[:, 2])
+
+
+# ax2.grid(linestyle='dashed')
 plt.show()
